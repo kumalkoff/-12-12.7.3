b0b4843239d44c1a822d4b23a15281633aca47ef
kumalkoff <120364432+kumalkoff@users.noreply.github.com>
2023-02-12 14:52:29 +0300

    Add files via upload

---
+++ Задание модуля 12-12.7.3.py
@@ -0,0 +1,11 @@
+money = int(input("сумма инвестирования:"))
+per_cent = ("ТКБ": 5,6. "VTB": 4,28, "СКБ": 5,9, "СБЕР" : 4,0)
+procent = list (per_cent.values())
+bankTKB = round(procent[0]/100*5000)
+bankVTB = round(procent[1]/100*5000)
+bankSKB = round(procent[2]/100*5000)
+bankSBER = round(procent[3]/100*5000)
+deposit = [bankTKB, bankVTB, bankSKB, bankSBER]
+print("2000", deposit)
+deposit.sort()
+print("max", deposit [0])
